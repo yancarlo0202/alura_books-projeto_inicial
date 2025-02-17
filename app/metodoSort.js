7346cd3b9f559b1e40c579ea4b225c200f676b27
yancarlo0202 <139280488+yancarlo0202@users.noreply.github.com>
2025-02-17 15:47:51 -0400

    corrigindo operador no metodo sort

--- app/metodoSort.js
+++ app/metodoSort.js
@@ -2,6 +2,6 @@ let btnOrdenarPorPreco = document.getElementById('btnOrdenarPorPreco')
 btnOrdenarPorPreco.addEventListener('click', ordenarLivroPorPreco)
 
 function ordenarLivroPorPreco() {
-    let livrosOrdenados = livros.sort((a, b) => a.preco = b.preco)
+    let livrosOrdenados = livros.sort((a, b) => a.preco - b.preco)
     exibirOsLivrosNaTela(livrosOrdenados)
 }
